4ba5aecdc252d0af323d4f8dfb20c435cdae1079
AvaBakuei <ava.bakuie@gmail.com>
2021-04-15 18:23:03 +0430

    edit loading

--- 001-user/src/app.js
+++ 001-user/src/app.js
@@ -1,26 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserList from "./pages/userList";
 import User from "./pages/user";
-import Loading from './utils/helpers/Loading';
 
 const App = () => {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-       setLoading(false);
-    }, []);
-
     return(
         <div className="app">
-            {loading ? <Loading/> : (
-                <Router>
-                    <Switch>
-                        <Route path={"/"} exact component={UserList} />
-                        <Route path={"/user/:id"} component={User} />
-                    </Switch>
-                </Router>
-            )}
+            <Router>
+                <Switch>
+                    <Route path={"/"} exact component={UserList} />
+                    <Route path={"/user/:id"} component={User} />
+                </Switch>
+            </Router>
         </div>
     )
 }
--- 001-user/src/pages/user/index.js
+++ 001-user/src/pages/user/index.js
@@ -2,33 +2,34 @@ import React, { useState, useEffect} from "react";
 import Skeleton from 'react-loading-skeleton';
 import axios from 'axios';
 import isEmpty from "../../utils/helpers/isEmpty";
-import Loading from "../../utils/helpers/Loading";
 
 const User = (props) => {
     const userId = props.match.params.id;
     const [state, setState] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
-    const [loading, setLoading] = useState(true);
+    const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
             .then((response) => {
-                setState(response.data);
-                setLoading(false);
-                setIsLoading(false);
+                if(response.status === 200) {
+                    if(isLoading) {
+                        setTimeout(() => {
+                            setLoading(false);
+                            setState(response.data);
+                        }, 3000);
+                    }
+                }
             })
     }, []);
     return(
         <div>
-            {loading ? <Loading/> : (
-                !isLoading && isEmpty(state)
-                    ? <span>User is Not Found!</span> : (
-                        <div>
-                            <div>{isLoading ? <Skeleton/> : state.name}</div>
-                            <div>{isLoading ? <Skeleton/> : state.username}</div>
-                            <div>{isLoading ? <Skeleton/> : state.address?.city}</div>
-                        </div>
-                    )
+            {!isLoading && isEmpty(state)
+                ? <span>User is Not Found!</span> : (
+                    <div>
+                        <div>{isLoading ? <Skeleton/> : state.name}</div>
+                        <div>{isLoading ? <Skeleton/> : state.username}</div>
+                        <div>{isLoading ? <Skeleton/> : state.address?.city}</div>
+                    </div>
             )}
         </div>
     )
